3a30aaeb63607d8ee0a6e666a11b0b8c7e99599b
Shruthi K N <shruthikngowda@gmail.com>
2019-12-13 10:20:18 +0000

    Added all the conditions to play a game

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 import random
-
-computer_input_list = ["rock", "paper", "scissors"]
+ROCK = "rock"
+PAPER = "paper"
+SCISSORS = "scissors"
+computer_input_list = [ROCK, PAPER, SCISSORS]
 computer_input = random.choice(computer_input_list)
-print(computer_input)
-
+#print(computer_input)
+comp = ""
+if(computer_input == ROCK):
+  comp = 1;
+elif(computer_input == SCISSORS):
+  comp = 2
+else:
+  comp = 3
 user_input = input("Enter your choice - rock, paper, scissors : ")
-print(user_input)
-if(computer_input == user_input):
+user_choice = ""
+if(user_input == ROCK):
+  user_choice = 1
+elif(user_input == SCISSORS):
+  user_choice = 2
+else:
+  user_choice = 3
+#print(user_input)
+print("Your opponent opted : ", computer_input)
+if(comp == user_choice):
   print("its a tie")
-elif
+elif(comp == 1 and user_choice == 2):
+  print("Sorry , you loose")
+elif(comp == 2 and user_choice == 3):
+  print("Sorry , you loose")
+elif(comp == 3 and user_choice == 1):
+  print("Sorry , you loose")
+else:
+  print("You won")
